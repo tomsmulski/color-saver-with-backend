eebe5e676881f366f1addbef24203d73313ab56c
Thomas Smulski <info@makenci.de>
2022-07-08 14:04:53 +0200

    save and load user Palettes

--- api/users/login.js
+++ api/users/login.js
@@ -2,28 +2,26 @@ import { User } from "../_database/models/index";
 import connectToMongodb from "../_database/connect-to-mongodb";
 
 const handler = async (request, response) => {
-
-  try{
+  try {
     await connectToMongodb();
 
-    if(request.method === "POST"){
+    if (request.method === "POST") {
+      const userNickName = request.body.nickName;
 
-        const userNickName = request.body.nickName;     
-        
-        if(userNickName !== ""){
-            const user = await User.findOne({nickName: userNickName});
+      if (userNickName !== "") {
+        const user = await User.findOne({ nickName: userNickName });
 
-            if(user != null){
-                return response.status(200).json(user);
-            }else{
-                return response.status(501).json({message:"User no Exist"})
-            }
-        }        
+        if (user != null) {
+          return response.status(200).json(user);
+        } else {
+          return response.status(501).json({ message: "User no Exist" });
+        }
+      }
     }
-
-  }catch(error){
-    return response.status(500).json({message:"Server Error" + error.message})
+  } catch (error) {
+    return response
+      .status(500)
+      .json({ message: "Server Error" + error.message });
   }
-
 };
 export default handler;
